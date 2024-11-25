1447855392ced0e0b683a58a59f040d2d7df6cc6
shashnaidu11 <122309627+shashnaidu11@users.noreply.github.com>
2024-11-25 17:54:22 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#tuples
+
+fruits=("apple","orange","pineapple","mango")
+print(fruits)
+
+#acsess value of a tuple
+print(fruits[1])
+#packing in tuple
+tuple=("My","Name","is","Shashwat")
+for item in tuple:
+    print(item,end=" ")
